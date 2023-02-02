4cd6647023b7d4c6e16fceab6f831000f8906f4d
Ha-Mundo <trauzzu@gmail.com>
2023-02-02 21:42:50 +0000

    initial JavaScript added

---
+++ scripts.js
@@ -0,0 +1,46 @@
+const loadData = async () => {
+  try {
+    const url = `http://substantiveresearch.pythonanywhere.com/`;
+    const res = await fetch(url);
+    console.log(res.ok);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+(async () => {
+  const data = await loadData();
+  const totalItems = data.length;
+
+  const sectorsList = data.map(element => element.name);
+  const uniqueSectorsList = [...new Set(sectorsList)];
+
+  let out = "";
+  let placeholder = document.querySelector("#data-output");
+
+  uniqueSectorsList.forEach(currSector => {
+    const numItems = sectorsList.filter(sector => sector === currSector);
+    console.log(numItems.length);
+
+    let percentages = parseFloat((numItems.length * 100) / totalItems).toFixed(
+      2
+    );
+
+    console.log(`sector ${currSector} represents ${percentages}%`);
+
+    out += `
+    <tr>
+      <th>
+        ${currSector}
+      </th>
+      <th>
+        ${percentages}%
+      </th>
+    </tr>
+`;
+  });
+
+  placeholder.innerHTML = out;
+})();
